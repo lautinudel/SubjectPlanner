d7753172d3b2778c067e854a83dbfea52ca025e9
Lautaro <lautaronudel@gmail.com>
2019-01-30 20:32:00 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/utn/frsf/isi/subjectplanner/subjectplanner/AsignaturaActivity.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsf.isi.subjectplanner.subjectplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsignaturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asignatura);
+    }
+}
