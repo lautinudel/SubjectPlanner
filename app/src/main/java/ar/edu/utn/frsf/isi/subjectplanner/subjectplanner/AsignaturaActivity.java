307a6af1e9a11d33101a40e66801a54896321835
Unknown <mateochamorro@hotmail.com>
2019-01-31 01:33:07 -0300

    Avance funcionalidad 1
    Falta añadir adaptador para ListView e ícono para desplegar un Drawer

--- app/src/main/java/ar/edu/utn/frsf/isi/subjectplanner/subjectplanner/AsignaturaActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/subjectplanner/subjectplanner/AsignaturaActivity.java
@@ -1,13 +1,38 @@
 package ar.edu.utn.frsf.isi.subjectplanner.subjectplanner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AsignaturaActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_asignatura);
+        //Cofiguramos el toolbar como action bar principal con el titulo correspondiente al activity.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Asignaturas");
+
+}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, "A implementar", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
 }
