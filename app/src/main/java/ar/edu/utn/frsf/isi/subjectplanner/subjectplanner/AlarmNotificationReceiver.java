f7e259b60d9b3181b5b07cc4f0442e0b0e5a16f7
Jean Pierre Saint Martin <jeansaintmartin@hotmail.com>
2019-02-27 19:28:11 -0300

    Solucionado error alarma en Android 8 y 9.

--- app/src/main/java/ar/edu/utn/frsf/isi/subjectplanner/subjectplanner/AlarmNotificationReceiver.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/subjectplanner/subjectplanner/AlarmNotificationReceiver.java
@@ -1,10 +1,12 @@
 package ar.edu.utn.frsf.isi.subjectplanner.subjectplanner;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 
 import ar.edu.utn.frsf.isi.subjectplanner.subjectplanner.Modelo.Tarea;
@@ -13,22 +15,37 @@ public class AlarmNotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+                NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+                String CHANNEL_ID = "alarma";
+                CharSequence name = context.getResources().getString(R.string.app_name);
+                int importance = NotificationManager.IMPORTANCE_HIGH;
 
-        builder.setAutoCancel(true).
-                setDefaults(Notification.DEFAULT_ALL)
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.drawable.ic_error_black_24dp)
-                .setContentTitle(intent.getStringExtra("nombre"))
-                .setContentText("Tiene una actividad pendiente")
-                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND);
+                if (android.os.Build.VERSION.SDK_INT >= 26) {
+                    NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+                    notificationManager.createNotificationChannel(mChannel);
+                    builder.setAutoCancel(true).
+                    setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_error_black_24dp)
+                    .setContentTitle(intent.getStringExtra("nombre"))
+                    .setContentText("Tiene una actividad pendiente")
+                    .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                    .setChannelId(CHANNEL_ID);
+        }
+        else {
+            builder.setAutoCancel(true).
+                    setDefaults(Notification.DEFAULT_ALL)
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_error_black_24dp)
+                    .setContentTitle(intent.getStringExtra("nombre"))
+                    .setContentText("Tiene una actividad pendiente")
+                    .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND);
 
-        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
         notificationManager.notify(1, builder.build());
 
-
-
     }
-
 
 }
